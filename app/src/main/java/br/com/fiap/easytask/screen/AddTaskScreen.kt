f00025b21a0be6040839322e64ce4fbc4967a18e
juliocoelho88 <52672733+juliocoelho88@users.noreply.github.com>
2024-06-14 11:18:00 -0300

    Update AddTaskScreen.kt
    Add algumas estilizações

--- app/src/main/java/br/com/fiap/easytask/screen/AddTaskScreen.kt
+++ app/src/main/java/br/com/fiap/easytask/screen/AddTaskScreen.kt
@@ -1,15 +1,10 @@
 package br.com.fiap.easytask.ui
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -23,33 +18,40 @@ fun AddTaskScreen(navController: NavController, taskViewModel: TaskViewModel) {
     var dueDate by remember { mutableStateOf("") }
     var status by remember { mutableStateOf("") }
 
-    Column(modifier = Modifier.padding(16.dp)) {
+    Column(modifier = Modifier
+        .padding(16.dp)
+        .fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
         TextField(
             value = title,
             onValueChange = { title = it },
-            label = { Text("Title") }
+            label = { Text("Título") }
         )
         TextField(
             value = description,
             onValueChange = { description = it },
-            label = { Text("Description") }
+            label = { Text("Descrição") }
         )
         TextField(
             value = dueDate,
             onValueChange = { dueDate = it },
-            label = { Text("Due Date") }
+            label = { Text("Data") }
         )
         TextField(
             value = status,
             onValueChange = { status = it },
             label = { Text("Status") }
         )
-        Button(onClick = {
-            val newTask = Task(0, title, description, dueDate, status)
-            taskViewModel.createTask(newTask)
-            navController.popBackStack()
-        }) {
-            Text("Add Task")
+        Button(
+            onClick = {
+                val newTask = Task(0, title, description, dueDate, status)
+                taskViewModel.createTask(newTask)
+                navController.popBackStack()
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Adicionar Tarefa")
         }
     }
 }

98db7857bcae5a7d424431cdaa7359a51e75b47d
juliocoelho88 <52672733+juliocoelho88@users.noreply.github.com>
2024-06-14 11:18:30 -0300

    Update TaskListScreen.kt
    add estilizações

--- app/src/main/java/br/com/fiap/easytask/screen/TaskListScreen.kt
+++ app/src/main/java/br/com/fiap/easytask/screen/TaskListScreen.kt
@@ -1,16 +1,23 @@
 package br.com.fiap.easytask.ui
 
-import TaskItem
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import br.com.fiap.easytask.TaskItem
 import br.com.fiap.easytask.model.TaskViewModel
 
 @Composable
@@ -21,14 +28,42 @@ fun TaskListScreen(taskViewModel: TaskViewModel, navController: NavController) {
         taskViewModel.fetchTasks()
     }
 
-    Column {
-        Button(onClick = { navController.navigate("addTask") }) {
-            Text("Add New Task")
-        }
-        LazyColumn {
-            items(tasks) { task ->
-                TaskItem(task, navController, taskViewModel)
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = { navController.navigate("addTask") }) {
+                Icon(Icons.Default.Add, contentDescription = "Adicionar Tarefa")
+            }
+        },
+        content = { innerPadding ->
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding),
+                contentAlignment = Alignment.Center
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Text(
+                        text = "Task Manager",
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                    LazyColumn(
+                        modifier = Modifier.fillMaxSize(),
+                        contentPadding = PaddingValues(16.dp),
+                        verticalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        items(tasks) { task ->
+                            TaskItem(task, navController, taskViewModel)
+                        }
+                    }
+                }
             }
         }
-    }
+    )
 }
